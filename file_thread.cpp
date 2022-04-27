a3fed289eafe6717a364a118fb86db0af5eeb6ac
Timofei Pshenichniy <timofei.psheno@gmail.com>
2022-04-27 23:46:29 +0300

    Добавьте файлы проекта.

---
+++ file_thread.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void f_create(FILE* f, int begin, int* x)
+{
+    FILE* f_buf;
+    f_buf = fopen(string("test" +string(int(begin / 10))+ ".bin"), "ab");
+    fseek(f, begin, SEEK_SET);
+    for (int i = 0; i < 10; i++)
+        fwrite(&x[begin + i], sizeof(int), 1, f_buf);
+}
+
+
+int main()
+{
+    FILE* f;
+    f = fopen("test.bin", "ab");
+    int* x = new int[100];
+    for (int i = 0; i < 100; i++)
+        x[i] = i;
+    fwrite(x, sizeof(int), 100, f);
+    fseek(f, 0, SEEK_SET);
+    fread(x, sizeof(int), 1, f);
+    for (int i = 0; i < 1; i++)
+        cout << x[i] << " ";
+    fclose(f);
+}
+
