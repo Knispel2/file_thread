fa801c1f85c58ae36f8767b7da6910ba29f1288d
Timofei Pshenichniy <timofei.psheno@gmail.com>
2022-04-28 20:17:59 +0300

    join?

--- file_thread.cpp
+++ file_thread.cpp
@@ -1,19 +1,26 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <thread>
 #include <string>
 #include <mutex>
 
+
 using namespace std;
 
-void f_create(FILE* f, int begin, int* x, mutex test_mutex)
+void f_create(FILE* f, int begin, mutex & test_mutex)
 {
     FILE* f_buf;
-    f_buf = fopen(string("test" + to_string(int(begin / 10)) + ".bin").c_str(), "ab");
+    f_buf = fopen(string("test" + to_string(begin) + ".bin").c_str(), "ab");
+    int* buf = new int;
     fseek(f, begin, SEEK_SET);
-    lock_guard<mutex> guard(test_mutex);
+    test_mutex.lock();
     for (int i = 0; i < 10; i++)
-        fwrite(&x[begin + i], sizeof(int), 1, f_buf);
-
+    {
+        fread(buf, sizeof(int), 1, f);
+        fwrite(buf, sizeof(int), 1, f_buf);
+    }  
+    test_mutex.unlock();
+    fclose(f_buf);
 }
 
 
@@ -28,9 +35,9 @@ int main()
     mutex test_mutex;
     for (int i = 0; i < 10; i++)
     {
-        thread task(f_create, f, i, x, test_mutex);
-        task.detach();
-    }        
+        thread task( f_create, f, i, ref(test_mutex) );
+        task.join();
+    }    
     fclose(f);
 }
 
