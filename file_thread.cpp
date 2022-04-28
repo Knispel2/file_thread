b6a5808d31aae4b06c91a9d86d81ae647072c86a
Timofei Pshenichniy <timofei.psheno@gmail.com>
2022-04-28 21:01:19 +0300

    Конец

--- file_thread.cpp
+++ file_thread.cpp
@@ -7,37 +7,42 @@
 
 using namespace std;
 
-void f_create(FILE* f, int begin, mutex & test_mutex)
+void f_create(int begin, mutex& test_mutex)
 {
-    FILE* f_buf;
-    f_buf = fopen(string("test" + to_string(begin) + ".bin").c_str(), "ab");
+    FILE* f_buf, * f;
+    lock_guard <mutex> test(test_mutex);
+    f = fopen("test.bin", "rb");
+    f_buf = fopen(string("test" + to_string(begin) + ".bin").c_str(), "wb");
     int* buf = new int;
     fseek(f, begin, SEEK_SET);
-    test_mutex.lock();
+    fseek(f_buf, 0, SEEK_SET);
     for (int i = 0; i < 10; i++)
     {
         fread(buf, sizeof(int), 1, f);
         fwrite(buf, sizeof(int), 1, f_buf);
-    }  
-    test_mutex.unlock();
+    }
     fclose(f_buf);
+    fclose(f);
+    delete buf;
 }
 
 
 int main()
 {
     FILE* f;
-    f = fopen("test.bin", "ab");
+    f = fopen("test.bin", "wb");
     int* x = new int[100];
     for (int i = 0; i < 100; i++)
         x[i] = i;
     fwrite(x, sizeof(int), 100, f);
     mutex test_mutex;
+    fclose(f);
     for (int i = 0; i < 10; i++)
     {
-        thread task( f_create, f, i, ref(test_mutex) );
-        task.join();
-    }    
-    fclose(f);
-}
+        //f_create(i, test_mutex);
+        thread task(f_create, i, ref(test_mutex));
+        if (i == 9) task.join();
+        else task.detach();
+    }
 
+}
